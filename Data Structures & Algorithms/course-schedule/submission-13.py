4bd8be66ada1afc562ad149bcc11adcae2d0c138
Nikola Nikolić <128241176+NikolaNikolicc@users.noreply.github.com>
2026-04-04 14:01:35 +0200

    Add: course-schedule - submission-13

---
+++ Data Structures & Algorithms/course-schedule/submission-13.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+from typing import List
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adjList = defaultdict(list)
+
+        for prereq in prerequisites:
+            adjList[prereq[0]].append(prereq[1])
+
+        result =[]
+        visited = set()
+        def dfs(rootIdx):
+            if rootIdx in visited:
+                return False
+            
+            if rootIdx in adjList and adjList[rootIdx] == []:
+                return True
+
+            result.append(rootIdx)
+            visited.add(rootIdx)
+            for nei in adjList[rootIdx]:
+                if not dfs(nei):
+                    return False
+            visited.remove(rootIdx)
+            adjList[rootIdx] = []
+            return True
+
+        for i in range(numCourses):
+            if not dfs(i):
+                return False
+        print(result)
+        return True
