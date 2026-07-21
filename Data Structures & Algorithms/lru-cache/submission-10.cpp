afc1aa232e980ede73f60449d9b5730a9aad26ca
Nikola Nikolić <128241176+NikolaNikolicc@users.noreply.github.com>
2026-07-21 18:35:29 +0200

    Add: lru-cache - submission-10

---
+++ Data Structures & Algorithms/lru-cache/submission-10.cpp
@@ -0,0 +1,76 @@
+struct LRUNode {
+    int key;
+    int val;
+    LRUNode* prev;
+    LRUNode* next;
+
+    LRUNode(int k, int v, LRUNode* p = nullptr, LRUNode* n = nullptr): key(k), val(v), prev(p), next(n) {}
+    LRUNode(): key(-1), val(-1), next(nullptr), prev(nullptr) {}
+};
+
+class LRUCache {
+    int cap = 0, size = 0;
+    unordered_map<int, LRUNode*> elems;
+    LRUNode* head, * tail;
+
+    void unchainElem(LRUNode* node) {
+        LRUNode* prev = node->prev;
+        LRUNode* next = node->next;
+
+        if (prev) prev->next = next;
+        if (next) next->prev = prev;
+    }
+
+    void moveToFront(LRUNode* node) {
+        if (node->next == tail) return;
+        if (elems.count(node->key) > 0) unchainElem(node);
+        
+        tail->prev->next = node;
+        node->prev = tail->prev;
+        node->next = tail;
+        tail->prev = node;
+    }
+    
+public:
+    LRUCache(int capacity) {
+        cap = capacity;
+        head = new LRUNode();
+        tail = new LRUNode();
+        head->next = tail;
+        tail->prev = head;
+    }
+    
+    int get(int key) {
+        if (elems.count(key) > 0) {
+            // get element
+            LRUNode* elem = elems[key];
+            // move to front
+            moveToFront(elem);
+            // return value
+            return elem->val;
+        }
+        return -1;
+    }
+    
+    void put(int key, int value) {
+        LRUNode* elem;
+        if (elems.count(key) > 0) {
+            elem = elems[key];
+            moveToFront(elem);
+            elem->val = value;
+            return;
+        } 
+        if (size == cap) {
+            LRUNode* node = head->next;
+            elems.erase(node->key);
+            unchainElem(node);
+            delete node;
+            size--;
+        }
+        elem = new LRUNode(key, value, tail->prev, tail);
+        tail->prev->next = elem;
+        tail->prev = elem;
+        elems[key] = elem;
+        size++;
+    }
+};
