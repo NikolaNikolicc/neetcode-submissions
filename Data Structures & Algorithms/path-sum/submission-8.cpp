12cf39553ece06c950558094ddb72b57c0613a41
Nikola Nikolić <128241176+NikolaNikolicc@users.noreply.github.com>
2026-07-21 12:00:37 +0200

    Add: path-sum - submission-8

---
+++ Data Structures & Algorithms/path-sum/submission-8.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if (!root) return false;
+
+        targetSum -= root->val;
+        if (!root->left && !root->right) {
+            return targetSum == 0;
+        }
+        bool leftRes = false, rightRes = false;
+        
+        leftRes = hasPathSum(root->left, targetSum);
+        if (leftRes) return leftRes;
+        rightRes = hasPathSum(root->right, targetSum);
+        return rightRes;
+    }
+};
