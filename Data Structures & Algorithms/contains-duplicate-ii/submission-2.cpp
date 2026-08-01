4e28a81cae65a08bf8591b9637bcffa73f588a25
Nikola Nikolić <128241176+NikolaNikolicc@users.noreply.github.com>
2026-08-01 12:43:29 +0200

    Add: contains-duplicate-ii - submission-2

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_set<int> uset;
+
+        for(int i = 0; i < nums.size(); i++) {
+            if (i > k) uset.erase(nums[i - k - 1]);
+            if (uset.count(nums[i]) > 0) return true;
+            uset.insert(nums[i]);
+        }
+        return false;
+    }
+};
